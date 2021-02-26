040195d748c781450ded05abd95b3721947165f5
Rohit <rohit1coding@gmail.com>
2021-02-27 02:24:43 +0530

    ist changes

--- app.js
+++ app.js
@@ -21,5 +21,6 @@ if(process.env.NODE_ENV=="production"){
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
 }
+
 app.listen(PORT,()=>{console.log(`App is running on Port:${PORT}`)});
 
