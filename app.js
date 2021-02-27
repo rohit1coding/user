93dcf992e7484225a6b99589e4d27945b417694b
Rohit <rohit1coding@gmail.com>
2021-02-28 01:20:01 +0530

    make it better

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var mongoose= require('mongoose');
 const {MongoURI}=require('./cofig/keys');
 const router = require('./routh/auth');
 require('./models/user')
-const PORT= process.env.PORT || 3001;
+const PORT= process.env.PORT || 4000;
 mongoose.connect(MongoURI,{useNewUrlParser:true,useUnifiedTopology:true})
     .then(()=>{console.log("connected to Mongodb")})
     .catch((err)=>{console.log("Error:connecting Mongodb:",err)})
